c172270d6d7b3018f83573d8d042f025e48710ce
dwit-p <1panchaldwi@hdsb.ca>
2025-02-26 14:22:22 -0500

    added some variables

--- 1ST ASSIGNMENT.py
+++ 1ST ASSIGNMENT.py
@@ -1,6 +1,12 @@
-Y = 2
+I = 2
 S = "blue, green and red"
+B = True
+F = 1.01
 
-print("An example of an integer is", Y)
+print("Examples of types of variables")
 print()
+
+print("An example of an integer is", I)
 print("An example of a string is", str(S))
+print("An example of boolean is", B)
+print("An example of float is", F)
