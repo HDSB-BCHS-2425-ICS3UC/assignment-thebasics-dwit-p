0e043c00b4a50c8ba0eee7e9aba08d6c02845bba
dwit-p <1panchaldwi@hdsb.ca>
2025-02-26 20:30:51 -0500

    added math equations.

--- 1ST ASSIGNMENT.py
+++ 1ST ASSIGNMENT.py
@@ -1,12 +1,41 @@
+import math
+
+
 I = 2
 S = "blue, green and red"
 B = True
 F = 1.01
 
-print("Examples of types of variables")
+print("Examples of types of variables:")
 print()
 
 print("An example of an integer is", I)
 print("An example of a string is", str(S))
 print("An example of boolean is", B)
 print("An example of float is", F)
+print()
+print()
+
+print("Math equations:")
+print()
+
+addition = 2+9
+print("2+2 =",addition)
+
+subtraction = 11-9
+print("11-9 =", subtraction)
+
+multiplication = 2*8
+print("2x8 =", multiplication)
+
+division = 4/2
+print("4/2 =",division)
+
+division_f = 400.00/200.9
+print("400.00/200.05 =", division_f)
+
+root = math.sqrt(9)
+print ("The square root of 9 is", root)
+
+exponent = 2**2
+print("2 squared =", exponent)
